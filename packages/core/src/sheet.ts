8e5d6d52b9226c79d3467f9d307c7ee5a9d4f17d
Durgé Seerden <github@durge.dev>
2026-04-11 21:10:42 +0200

    Remove disable rule

--- packages/core/src/sheet.ts
+++ packages/core/src/sheet.ts
@@ -178,8 +178,7 @@ export const createSheet = (root: (DocumentOrShadowRoot & Node) | null): SheetGr
 	return groupSheet
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const noop = () => {}
+const noop = () => undefined
 
 const addApplyToGroup = (group: RuleGroup): void => {
 	const groupingRule = group.group
